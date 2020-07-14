b0af352b627760d4794e713d30e46eaf727750ae
midesweb <malvarez@desarrolloweb.com>
2020-07-14 17:36:28 +0200

    create button templates to reuse svg

--- packages/dile-date-without-calendar/src/DileDateWithoutCalendar.js
+++ packages/dile-date-without-calendar/src/DileDateWithoutCalendar.js
@@ -13,10 +13,7 @@ export class DileDateWithoutCalendar extends LitElement {
   }
 
   static get styles() {
-    return [css `
-        
-        <style>.{}
-        
+    return [css `      
         :host {
           display: inline-block;
           position: relative;
@@ -406,13 +403,9 @@ export class DileDateWithoutCalendar extends LitElement {
               
                 <div class="datetime-scroll-box-wrapper">
                    
-                    <button upbutton>
-                        <svg version="1.1" class="upbutton-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 493.258 493.258"><g><path d="M131.564,219.258h230.13c9.314,0,17.857-5.16,22.203-13.389c4.34-8.246,3.769-18.193-1.494-25.895 L265.92,9.772C261.75,3.68,254.846,0.033,247.461,0c-7.377-0.018-14.303,3.584-18.507,9.658L110.935,179.863 c-5.319,7.668-5.938,17.664-1.607,25.941C113.659,214.067,122.225,219.258,131.564,219.258z"/></g></svg>
-                    </button>
+                    ${this.upButtonTemplate}
 
-                    <button downbutton>
-                        <svg version="1.1" class="downbutton-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 493.258 493.258"><g><path d="M361.694,273.998h-230.13c-9.339,0-17.905,5.193-22.236,13.455c-4.331,8.277-3.711,18.273,1.607,25.939 l118.019,170.205c4.204,6.074,11.13,9.676,18.507,9.66c7.385-0.031,14.289-3.682,18.459-9.772l116.483-170.205 c5.263-7.699,5.834-17.648,1.494-25.895C379.551,279.158,371.008,273.998,361.694,273.998z"/></g></svg>
-                    </button>
+                    ${this.downButtonTemplate}
                 
                      <div datetimeScrollField tabindex="0" id="day">
                     
@@ -430,12 +423,8 @@ export class DileDateWithoutCalendar extends LitElement {
                 
                 <div class="datetime-scroll-box-wrapper">
                 
-                    <button upbutton>
-                        <svg version="1.1" class="upbutton-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 493.258 493.258"><g><path d="M131.564,219.258h230.13c9.314,0,17.857-5.16,22.203-13.389c4.34-8.246,3.769-18.193-1.494-25.895 L265.92,9.772C261.75,3.68,254.846,0.033,247.461,0c-7.377-0.018-14.303,3.584-18.507,9.658L110.935,179.863 c-5.319,7.668-5.938,17.664-1.607,25.941C113.659,214.067,122.225,219.258,131.564,219.258z"/></g></svg>
-                    </button>
-                    <button downbutton>
-                        <svg version="1.1" class="downbutton-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 493.258 493.258"><g><path d="M361.694,273.998h-230.13c-9.339,0-17.905,5.193-22.236,13.455c-4.331,8.277-3.711,18.273,1.607,25.939 l118.019,170.205c4.204,6.074,11.13,9.676,18.507,9.66c7.385-0.031,14.289-3.682,18.459-9.772l116.483-170.205 c5.263-7.699,5.834-17.648,1.494-25.895C379.551,279.158,371.008,273.998,361.694,273.998z"/></g></svg>
-                    </button>
+                    ${this.upButtonTemplate}
+                    ${this.downButtonTemplate}
                     
                     <div datetimeScrollField tabindex="0" id="month">
                         
@@ -454,13 +443,9 @@ export class DileDateWithoutCalendar extends LitElement {
 
                 <div class="datetime-scroll-box-wrapper" id="datePopupFieldYear">
                 
-                    <button upbutton>
-                        <svg version="1.1" class="upbutton-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 493.258 493.258"><g><path d="M131.564,219.258h230.13c9.314,0,17.857-5.16,22.203-13.389c4.34-8.246,3.769-18.193-1.494-25.895 L265.92,9.772C261.75,3.68,254.846,0.033,247.461,0c-7.377-0.018-14.303,3.584-18.507,9.658L110.935,179.863 c-5.319,7.668-5.938,17.664-1.607,25.941C113.659,214.067,122.225,219.258,131.564,219.258z"/></g></svg>
-                    </button>
+                    ${this.upButtonTemplate}
 
-                    <button downbutton>
-                        <svg version="1.1" class="downbutton-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 493.258 493.258"><g><path d="M361.694,273.998h-230.13c-9.339,0-17.905,5.193-22.236,13.455c-4.331,8.277-3.711,18.273,1.607,25.939 l118.019,170.205c4.204,6.074,11.13,9.676,18.507,9.66c7.385-0.031,14.289-3.682,18.459-9.772l116.483-170.205 c5.263-7.699,5.834-17.648,1.494-25.895C379.551,279.158,371.008,273.998,361.694,273.998z"/></g></svg>
-                    </button>
+                    ${this.downButtonTemplate}
                     
                     <div datetimeScrollField tabindex="0" id="year">
                     
@@ -477,6 +462,22 @@ export class DileDateWithoutCalendar extends LitElement {
         : ''}
     </div>`;
 
+  }
+
+  get upButtonTemplate() {
+    return html`
+      <button upbutton>
+        <svg version="1.1" class="upbutton-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 493.258 493.258"><g><path d="M131.564,219.258h230.13c9.314,0,17.857-5.16,22.203-13.389c4.34-8.246,3.769-18.193-1.494-25.895 L265.92,9.772C261.75,3.68,254.846,0.033,247.461,0c-7.377-0.018-14.303,3.584-18.507,9.658L110.935,179.863 c-5.319,7.668-5.938,17.664-1.607,25.941C113.659,214.067,122.225,219.258,131.564,219.258z"/></g></svg>
+      </button>
+    `;
+  }
+
+  get downButtonTemplate() {
+    return html`
+      <button downbutton>
+        <svg version="1.1" class="downbutton-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 493.258 493.258"><g><path d="M361.694,273.998h-230.13c-9.339,0-17.905,5.193-22.236,13.455c-4.331,8.277-3.711,18.273,1.607,25.939 l118.019,170.205c4.204,6.074,11.13,9.676,18.507,9.66c7.385-0.031,14.289-3.682,18.459-9.772l116.483-170.205 c5.263-7.699,5.834-17.648,1.494-25.895C379.551,279.158,371.008,273.998,361.694,273.998z"/></g></svg>
+      </button>
+    `
   }
 
   constructor(){
