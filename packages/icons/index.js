67a675a0ad2d699f2dc6148b1cd8a1273edc236d
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2022-05-24 00:20:54 +0200

    filter icon

--- packages/icons/index.js
+++ packages/icons/index.js
@@ -1,3 +1,4 @@
 export { iconStyles } from "./src/iconStyles.js";
 export { infoIcon } from "./src/info.js";
+export { filterIcon } from "./src/filter.js";
 export { closeIcon } from "./src/close.js";
