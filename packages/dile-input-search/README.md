afc86733dd6b9065010aa4d457e1a70f60c4644e
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2022-05-27 10:39:22 +0200

    add dile-search-input component

---
+++ packages/dile-input-search/README.md
@@ -0,0 +1,55 @@
+# @dile/dile-input-search
+
+Web Component to create a customized search input with the tipical search icon and a clear search functionality. Also it provides a "delay" property to wait when a keystroke occurs and when a search is emitted.
+
+This component does not search anything by itself. It's only a search user interface you may use to create a search functionality as you need. Instead of making a search the component emitt events with a keyword to tell others that the user want to perform a search action.
+
+## Installation
+```bash
+npm i @dile/dile-input-search
+```
+
+## Usage
+
+```html
+<script type="module">
+  import '@dile/dile-input-search/dile-input-search.js';
+</script>
+
+<dile-input-search></dile-input-search>
+```
+
+## Properties
+
+- **delay**: Miliseconds the component waits between when a keystroke occurs and when a search keyword is emitted.
+- **placeholder**: Placeholder text
+- **keyword**: the text typed into the input search component
+
+## Custom events
+
+- **dile-input-search**: A search is performed with a new keyword. The event detail is an object like this:
+
+```javascript
+{
+  keyword: "the text searched"
+}
+```
+
+## CSS Custom Properties
+
+You can customize it using CSS Custom Properties.
+
+Custom property | Description | Default
+----------------|-------------|---------
+--dile-icon-color | Icon color | 0.5rem
+--dile-icon-size | Icon size | 24px
+--dile-input-width | Input element width | 100%
+--dile-input-border-width | Input element border width | 1px
+--dile-input-border-color | Input element border color | #888
+--dile-input-border-radius | Input element border radius | 5px
+--dile-input-font-size | Input element font size | 1em
+--dile-input-line-height | Input element line height | 1.5em
+--dile-input-color | Input text color | #303030
+--dile-input-background-color | Color for the background input element | #fff
+--dile-input-padding | Padding for the input text | 5px
+--dile-input-placeholder-color | Placeholder color | #ccc
---
+++ packages/dile-input-search/src/DileInputSearch.js
@@ -0,0 +1,105 @@
+import { LitElement, html, css } from 'lit';
+import '@dile/dile-icon/dile-icon.js';
+import { searchIcon } from '@dile/icons';
+import { clearIcon } from '@dile/icons';
+
+export class DileInputSearch extends LitElement {
+    static styles = [
+        css`
+            :host {
+                display: block;
+            }
+            div {
+                display: flex;
+                box-sizing: border-box;
+                border-radius: var(--dile-input-border-radius, 5px);
+                border: var(--dile-input-border-width, 1px) solid var(--dile-input-border-color, #888);
+                padding: var(--dile-input-padding, 5px);
+                width: var(--dile-input-width, 100%);
+                background-color: var(--dile-input-background-color, #fff);
+            }
+            input {
+                flex-grow: 1;
+                border: none;
+                outline: none;
+                color: var(--dile-input-color, #303030);
+                font-size: var(--dile-input-font-size, 1em);
+                line-height: var(--dile-input-line-height, 1.5em);
+                background-color: var(--dile-input-background-color, #fff);
+            }
+            input::placeholder {
+              color: var(--dile-input-placeholder-color, #ccc);
+            }
+            dile-icon {
+                margin-left: 1px;
+                display: flex;
+            }
+        `
+    ];
+
+    static get properties() {
+      return {
+        delay: { type: Number },
+        placeholder: { type: String },
+        value: { type: String }
+      };
+    }
+
+    constructor() {
+        super();
+        this.delay = 300;
+        this.timeout = null;
+        this.value = '';
+    }
+
+    firstUpdated() {
+        this.input = this.shadowRoot.getElementById('elinput');
+    }
+
+    render() {
+        return html`
+        <div>
+            <input 
+                id="elinput"
+                type="text" 
+                placeholder="${this.placeholder}" 
+                @input=${this.inputHandler}
+                autocomplete="off"
+                .value="${this.value}"
+            >
+            <dile-icon 
+              @click=${this.iconClick}
+              .icon="${this.value.length ? clearIcon : searchIcon}"
+            ></dile-icon>
+        </div>
+        `;
+    }
+
+    inputHandler(e) {
+        this.value = e.target.value;
+        if (this.timeout) {
+            clearTimeout(this.timeout);
+        }
+        this.timeout = setTimeout(() => {
+            this.dispatchSearch(this.value);
+        }, this.delay);
+    }
+
+    iconClick(e) {
+        if(this.value.length) {
+            this.value = '';
+            this.dispatchSearch(this.value);
+        }
+        this.input.focus();
+    }
+
+    dispatchSearch(key) {
+        this.dispatchEvent(new CustomEvent('dile-input-search', {
+            bubbles: true,
+            composed: true,
+            detail: {
+                keyword: key
+            }
+        }));
+    }
+}
