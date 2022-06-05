01430c431d14520f06a189af3fc08e5cedcbea9d
midesweb <malvarez@desarrolloweb.com>
2022-06-05 19:24:43 +0200

    create component

---
+++ packages/dile-select/src/DileSelectAjax.js
@@ -0,0 +1,28 @@
+import { LitElement, html, css } from 'lit';
+
+class DIleSElectAJax  extends LitElement {
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+    `;
+  }
+
+  static get properties() {
+    return {};
+  }
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      
+    `;
+  }
+}
+
+customElements.define('DileSelectAjax', DIleSElectAJax);
