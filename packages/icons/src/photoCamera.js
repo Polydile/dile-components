49f1012ee7aec5277bfcbb51aa59340263221249
midesweb <malvarez@desarrolloweb.com>
2022-07-26 20:36:01 +0200

    add new icons

---
+++ packages/icons/src/photoCamera.js
@@ -0,0 +1,2 @@
+import { html } from 'lit';
+export const photoCameraIcon = html`<svg class="dile-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"></path><path d="M8.8,12a3.2,3.2 0 1,0 6.4,0a3.2,3.2 0 1,0 -6.4,0"></path></circle><path d="M9 2L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-3.17L15 2H9zm3 15c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"></path></svg>`;
---
+++ packages/icons/src/receiptLong.js
@@ -0,0 +1,2 @@
+import { html } from 'lit';
+export const receiptLongIcon = html`<svg class="dile-icon" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0,0h24v24H0V0z" fill="none"/><g><path d="M19.5,3.5L18,2l-1.5,1.5L15,2l-1.5,1.5L12,2l-1.5,1.5L9,2L7.5,3.5L6,2v14H3v3c0,1.66,1.34,3,3,3h12c1.66,0,3-1.34,3-3V2 L19.5,3.5z M19,19c0,0.55-0.45,1-1,1s-1-0.45-1-1v-3H8V5h11V19z"/><path d="M9,7h6v2h-6Z" /><path d="M16,7h2v2h-2Z" /><path d="M9,10h6v2h-6Z" /><path d="M16,10h2v2h-2Z" /></g></svg>`;
