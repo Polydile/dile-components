4bc93311c93e3f8e2786aea97d18235435154e18
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2023-06-21 18:22:07 +0200

    back to previous menu plugin

--- packages/dile-editor/src/prosemirror/menu-plugin.js
+++ packages/dile-editor/src/prosemirror/menu-plugin.js
@@ -1,20 +1,18 @@
 import { Plugin } from "prosemirror-state";
 
-export const createMenuPlugin = () => {
-  const toolbarElement = 'dile-editor-toolbar';
-  const toolbar = document.createElement(toolbarElement);
-  
-  return new Plugin({
-    view(editorView) {
-      if (!editorView.dom.parentElement.querySelector(toolbarElement)) {
-        toolbar.editorView = editorView;
-        editorView.dom.parentNode.insertBefore(toolbar, editorView.dom);
-      }
-      return {
-        update() {
-          toolbar.reviewActiveElements();
-        }
-      };
+const toolbarElement = 'dile-editor-toolbar';
+const toolbar = document.createElement(toolbarElement);
+
+export const menuPlugin = new Plugin({
+  view(editorView) {
+    if (!editorView.dom.parentElement.querySelector(toolbarElement)) {
+      toolbar.editorView = editorView;
+      editorView.dom.parentNode.insertBefore(toolbar, editorView.dom);
     }
-  });
-}
+    return {
+      update() {
+        toolbar.reviewActiveElements();
+      }
+    };
+  }
+});
