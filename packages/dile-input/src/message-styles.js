6910804fccff1c8677957c464799c6dafa12bcbc
midesweb <malvarez@desarrolloweb.com>
2023-09-28 09:51:24 +0200

    change em to rem

--- packages/dile-input/src/message-styles.js
+++ packages/dile-input/src/message-styles.js
@@ -4,7 +4,7 @@ export const messageStyles = css`
   .message span {
     display: block;
     padding-top: var(--dile-input-message-padding-top, 4px);
-    font-size: var(--dile-input-message-font-size, 0.875em);
+    font-size: var(--dile-input-message-font-size, 0.875rem);
     color: var(--dile-input-message-color, #888);
 
   }
