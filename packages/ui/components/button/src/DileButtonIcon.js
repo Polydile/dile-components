d2664f8e77a118073f029d08383a78339f2b3f17
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2024-10-18 14:53:41 +0200

    change custom css property

--- packages/ui/components/button/src/DileButtonIcon.js
+++ packages/ui/components/button/src/DileButtonIcon.js
@@ -16,7 +16,7 @@ export class DileButtonIcon extends DileButton {
         button {
           display: flex;
           align-items: center;
-          --dile-icon-color: var(--dile-icon-button-color, var(--dile-on-primary-color, #ffffff));
+          --dile-icon-color: var(--dile-button-icon-color, var(--dile-on-primary-color, #ffffff));
         }
         dile-icon {
           margin-right: var(--dile-button-icon-separation, 0.3rem);
