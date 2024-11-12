24a64a83284f3587ac64756a0c7aef0a1495c2ab
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2024-11-12 19:29:32 +0100

    add extension to import

--- packages/editor/src/DileI18nMixin.js
+++ packages/editor/src/DileI18nMixin.js
@@ -1,5 +1,5 @@
 import { DileI18nMixin as OriginalDileI18nMixin } from '@dile/ui/mixins/i18n/DileI18nMixin.js';
-import { translationService } from './translationService';
+import { translationService } from './translationService.js';
 
 export const DileI18nMixin = (superclass) => 
   class extends OriginalDileI18nMixin(translationService)(superclass) {
