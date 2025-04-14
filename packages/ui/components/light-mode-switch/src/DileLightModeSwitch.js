60c7decc2d95169ea402296241723d0bb5732c3f
midesweb <malvarez@desarrolloweb.com>
2025-04-14 12:16:04 +0200

    add custom properties for focus state

--- packages/ui/components/light-mode-switch/src/DileLightModeSwitch.js
+++ packages/ui/components/light-mode-switch/src/DileLightModeSwitch.js
@@ -12,15 +12,21 @@ export class DileLightModeSwitch extends LitElement {
         --dile-switch-on-state-color: var(--dile-light-mode-switch-dark-state-color, #e5e5e5);
         --dile-switch-bar-color: var(--dile-light-mode-switch-light-state-bar-color, #dddddd);
         --dile-icon-color: var(--dile-light-mode-switch-light-state-icon-color, #303030);
+        --dile-switch-off-focus-color: var(--dile-light-mode-switch-light-state-focus-color, #3d91e5);
       }
       :host([darkMode]) {
-        --dile-switch-bar-color: var(--dile-light-mode-switch-dark-state-bar-color, #0e0e0e);
+        --dile-switch-bar-color: var(--dile-light-mode-switch-dark-state-bar-color, #6c6c6c);
         --dile-icon-color: var(--dile-light-mode-switch-dark-state-icon-color, #e5e5e5);
+        --dile-switch-on-focus-color: var(--dile-light-mode-switch-dark-state-focus-color, #ff9);
       }
+
       section {
         display: flex;
         align-items: center;
         gap: 0.5rem;
+      }
+      dile-icon {
+        cursor: pointer;
       }
     `;
   }
@@ -30,18 +36,28 @@ export class DileLightModeSwitch extends LitElement {
       darkMode: { type: Boolean, reflect: true },
     };
   }
+  
   constructor() {
     super();
     this.darkModeClass = "dark-theme";
-    this.darkMode = document.documentElement.classList.contains(this.darkModeClass);
+    const storedMode = localStorage.getItem("dile_light_mode_switch_state");
+    if (storedMode === "dark") {
+      this.darkMode = true;
+    } else if (storedMode === "light") {
+      this.darkMode = false;
+    } else {
+      this.darkMode = document.documentElement.classList.contains(this.darkModeClass);
+    }
   }
 
   updated(changedProperties) {
     if(changedProperties.has('darkMode')) {
       if(this.darkMode) {
         document.documentElement.classList.add(this.darkModeClass);
+        localStorage.setItem("dile_light_mode_switch_state", "dark");
       } else {
         document.documentElement.classList.remove(this.darkModeClass);
+        localStorage.setItem("dile_light_mode_switch_state", "light");
       }
     }
   }
@@ -49,7 +65,7 @@ export class DileLightModeSwitch extends LitElement {
   render() {
     return html`
       <section>
-        <dile-icon .icon="${this.darkMode ? darkMode : lightMode }"></dile-icon>
+        <dile-icon .icon="${this.darkMode ? darkMode : lightMode }" @click=${this.toggle}></dile-icon>
         <dile-switch ?checked="${this.darkMode}" @element-changed=${this.change}></dile-switch>
       </section>
     `;
