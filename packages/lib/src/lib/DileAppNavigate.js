e612c77a35a72f9635279aadad84ce7e35e3bc42
midesweb <malvarez@desarrolloweb.com>
2025-05-14 11:55:56 +0200

    remove console.log

--- packages/lib/src/lib/DileAppNavigate.js
+++ packages/lib/src/lib/DileAppNavigate.js
@@ -1,6 +1,5 @@
 export const DileAppNavigate = (superclass) => class extends superclass {
   goToUrl(url, title = '') {
-    console.log('app navigate', url, title)
     document.dispatchEvent(new CustomEvent('dile-lib-navigate', {
       bubbles: true,
       composed: true,
