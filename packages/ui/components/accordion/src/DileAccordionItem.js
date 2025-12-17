dd209be80cf5f22afaad875215767ffbcdac8e1f
aldaydev <aldaydev@gmail.com>
2025-12-17 16:38:26 +0100

    dile-accordion done

--- packages/ui/components/accordion/src/DileAccordionItem.js
+++ packages/ui/components/accordion/src/DileAccordionItem.js
@@ -15,29 +15,30 @@ export class DileAccordionItem extends DileSlideDown(LitElement) {
 
         /** Container */
         width: var(--dile-accordion-item-width, 100%);
+        border-radius: var(--dile-accordion-item-border-radius, none);
 
         /** Others */
         color: var(--dile-accordion-item-color, red);
         background-color: var(--dile-accordion-item-background-color, transparent);
 
-        /* overflow: hidden; */
+        overflow: hidden;
       }
 
       button{
         /** Container */
-        width: 100%;
+        width: var(--dile-accordion-item-width, 100%);
         height: 100%;
         padding: var(--dile-accordion-item-button-padding, .7rem);
-        border: var(--dile-accordion-accordion-item-button-border, none);
-        border-radius: var(--dile-accordion-accordion-item-button-border-radius, .5rem);
-        box-shadow: var(--dile-accordion-accordion-item-button-box-shadow, 2px 2px 10px gray);
+        border: var(--dile-accordion-item-button-border, none);
+        border-radius: var(--dile-accordion-item-border-radius, var(--dile-accordion-item-button-border-radius, .5rem));
+        box-shadow: var(--dile-accordion-item-button-box-shadow, 2px 2px 10px gray);
         margin-bottom: 0;
         transition: margin-bottom .2s .3s ease-in-out;
 
         /** Others */
         font-size: var(--dile-accordion-item-button-font-size, 1.1rem);
         background: var(--dile-accordion-item-button-background-color, black);
-        color: var(--dile-accordion-accordion-item-button-color, var(--dile-accordion-item-color, white));
+        color: var(--dile-accordion-item-button-color, var(--dile-accordion-item-color, white));
         cursor: pointer;
 
       }
@@ -71,16 +72,16 @@ export class DileAccordionItem extends DileSlideDown(LitElement) {
 
       .contentContainer{
         /** Container */
-        width: 100%;
+        width: var(--dile-accordion-item-width, 100%);
         height: 0;
         overflow: hidden;
 
         /** Others */
-        border: var(--dile-accordion-accordion-item-content-border, none);
-        border-radius: var(--dile-accordion-accordion-item-content-border-radius, .5rem);
-        box-shadow: var(--dile-accordion-accordion-item-content-box-shadow, 2px 2px 10px gray);
+        border: var(--dile-accordion-item-content-border, none);
+        border-radius: var(--dile-accordion-item-border-radius, var(--dile-accordion-item-content-border-radius, .5rem));
+        box-shadow: var(--dile-accordion-item-content-box-shadow, 2px 2px 10px gray);
         background: var(--dile-accordion-item-content-background-color, black);
-        color: var(--dile-accordion-accordion-content-color, var(--dile-accordion-item-color, white));
+        color: var(--dile-accordion-content-color, var(--dile-accordion-item-color, white));
         transition: height .3s ease-in-out;
       }
 
@@ -100,20 +101,20 @@ export class DileAccordionItem extends DileSlideDown(LitElement) {
   static properties = {
     title: { type: String, reflect: true },
     opened: { type: Boolean, reflect: true },
-    _height: { type: Number, state: true},
   };
 
   constructor(){
     super();
     this.opened = false;
-    this._height = 0;
     this.title = '';
-    this._calculatedHeight = 0;
     this.expandableContent = null;
+
+    if(this.hasAttribute('dile-cloak')) this.removeAttribute('dile-cloak');
   };
 
   firstUpdated(){
     this.expandableContent = this.shadowRoot.getElementById('content');
+
   }
 
   updated(changed){
