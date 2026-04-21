86bcff8bc347f9846ae8767fdc043136b768ae68
midesweb <malvarez@desarrolloweb.com>
2026-04-21 19:23:34 +0200

    remove internal element to easy styling

--- packages/ui/components/card/src/DileCard.js
+++ packages/ui/components/card/src/DileCard.js
@@ -8,14 +8,10 @@ export class DileCard extends LitElement {
         }
         :host {
           display: block;
-          align-items: stretch;
-        }
-        section {
           border: var(--dile-card-border, 1px solid #ccc);
           border-radius: var(--dile-card-border-radius, 0.5rem);
           background-color: var(--dile-card-background-color, #fff);
           color: var(--dile-card-text-color, #303030);
-          width: 100%;
           text-align: var(--dile-card-text-align, left);
           font-weight: var(--dile-card-font-weight, normal);
           box-shadow: var(--dile-card-box-shadow, 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1));
@@ -45,22 +41,22 @@ export class DileCard extends LitElement {
           border-bottom-left-radius: var(--dile-card-border-radius, 0.5rem);
           border-bottom-right-radius: var(--dile-card-border-radius, 0.5rem);
         }
-        :host([shadow-none]) section {
+        :host([shadow-none]) {
           box-shadow: var(--dile-card-box-shadow, 0 0 #0000);
         }
-        :host([shadow-sm]) section {
+        :host([shadow-sm]) {
           box-shadow: var(--dile-card-box-shadow, 0 1px 2px 0 rgb(0 0 0 / 0.05));
         }
-        :host([shadow-md]) section {
+        :host([shadow-md]) {
           box-shadow: var(--dile-card-box-shadow, 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1));
         }
-        :host([shadow-lg]) section {
+        :host([shadow-lg]) {
           box-shadow: var(--dile-card-box-shadow, 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1));
         }
-        :host([shadow-xl]) section {
+        :host([shadow-xl]) {
           box-shadow: var(--dile-card-box-shadow, 0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1));
         }
-        :host([shadow-2xl]) section {
+        :host([shadow-2xl]) {
           box-shadow: var(--dile-card-box-shadow, 0 25px 50px -12px rgb(0 0 0 / 0.25));
         }
 
@@ -75,13 +71,11 @@ export class DileCard extends LitElement {
 
     render() {
       return html`
-        <section>
-          ${this.titleTemplate}
-          <main>
-            <slot></slot>
-          </main>
-          ${this.footerTemplate}
-        </section>
+        ${this.titleTemplate}
+        <main>
+          <slot></slot>
+        </main>
+        ${this.footerTemplate}
       `;
     }
 
