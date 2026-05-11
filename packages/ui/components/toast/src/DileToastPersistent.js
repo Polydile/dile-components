dfe531369cae2482a0d95c31b62551007949e94b
midesweb <malvarez@desarrolloweb.com>
2026-05-11 08:57:57 +0200

    rewrite of dile-toast-persistent component

--- packages/ui/components/toast/src/DileToastPersistent.js
+++ packages/ui/components/toast/src/DileToastPersistent.js
@@ -1,118 +1,131 @@
 import { LitElement, html, css } from "lit";
-import { DileOverlay } from "../../../mixins/overlay/index.js"
 
-export class DileToastPersistent extends DileOverlay(LitElement) {
+export class DileToastPersistent extends LitElement {
   static get styles() {
     return css`
       :host {
-        display: inline;
-        position: relative;
-        box-sizing: border-box;
-      }
-      span {
+        display: block;
         position: fixed;
-        bottom: 0px;
-        left: 0px;
-        
-        width: 0;
-        height: 0;
+        bottom: var(--dile-toast-persistent-gap, 1rem);
+        left: var(--dile-toast-persistent-gap, 1rem);
+        z-index: var(--dile-toast-persistent-z-index, 999);
+        width: var(--dile-toast-persistent-width, 300px);
+        box-sizing: border-box;
+        pointer-events: none;
       }
-      :host([opened]) section {
-        transform: translateX(0);
-      }
-      :host([right]) span {
-        right: 0px;
+      :host([right]) {
         left: auto;
+        right: var(--dile-toast-persistent-gap, 1rem);
       }
-      :host([center]:not([right])) span {
+      :host([center]:not([right])) {
         left: 50%;
-        right: auto;
+        transform: translateX(-50%);
       }
-      #overlay {
+      #toast {
+        pointer-events: all;
+        display: none;
+        opacity: 0;
+        transform: translateY(12px);
+        transition: opacity var(--dile-toast-persistent-transition, 0.5s ease),
+                    transform var(--dile-toast-persistent-transition, 0.5s ease);
         box-sizing: border-box;
         color: var(--dile-toast-persistent-color, #fff);
-        z-index: var(--dile-toast-persistent-z-index, 999);
         background-color: var(--dile-toast-persistent-background-color, #0e6ff5);
         border-radius: var(--dile-toast-persistent-border-radius, 0);
-        width: var(--dile-toast-persistent-width, 300px);
         max-width: var(--dile-toast-persistent-max-width, 300px);
         box-shadow: var(--dile-toast-persistent-box-shadow, 0 0 2px rgba(200, 200, 200, 0.5));
         padding: var(--dile-toast-persistent-padding, 1px);
         overflow: var(--dile-toast-persistent-overflow, visible);
-        display: none;
-        position: absolute;
-        opacity: 0;
-        transition: var(--dile-toast-persistent-transition, ease 0.5s);
-        transition-property: transform, opacity;
-        transform: translateY(-10px);
       }
-      #overlay.opened {
+      #toast.visible {
         opacity: 1;
         transform: translateY(0);
-      }
-      #trigger {
-        display: flex;
       }
     `;
   }
 
   static get properties() {
     return {
+      opened: {
+        type: Boolean,
+        reflect: true,
+      },
       right: {
         type: Boolean,
-        reflect: true
+        reflect: true,
       },
       center: {
         type: Boolean,
-        reflect: true
+        reflect: true,
       },
       openOnInit: {
         type: Boolean,
-      }
+      },
     };
   }
 
   constructor() {
     super();
-    this.horizontalAlign = 'left';
-    this.verticalAlign = 'top';
-    this.moveLeft = -10;
-    this.moveTop = -16;
+    this.opened = false;
+    this.right = false;
+    this.center = false;
+    this.openOnInit = false;
   }
 
   firstUpdated() {
-    super.firstUpdated();
-    if(this.openOnInit) {
-      setTimeout( () => this.open(), 200);
+    this._toastEl = this.shadowRoot.getElementById('toast');
+    if (this.openOnInit) {
+      setTimeout(() => this.open(), 200);
     }
   }
-  
+
   updated(changedProperties) {
-    if(changedProperties.has('right') || changedProperties.has('center')) {
-      if (this.right) {
-        this.horizontalAlign = 'left';
-        this.moveLeft = -10;
-      } else if (this.center) {
-        this.horizontalAlign = 'center';
-        this.moveLeft = 0;
-      } else {
-        this.horizontalAlign = 'right';
-        this.moveLeft = -10;
+    if (changedProperties.has('opened')) {
+      if (this.opened) {
+        this._show();
+      } else if (changedProperties.get('opened') !== undefined) {
+        this._hide();
       }
     }
   }
 
-  render() {
-    return html`
-      <span id="trigger">
-        <div
-          id="overlay"
-          class="${this._overlayClass}"
-        >
-          <slot></slot>
-        </div>
-      </span>
-    `;
+  open() {
+    this.opened = true;
   }
 
+  close() {
+    this.opened = false;
+  }
+
+  toggle() {
+    this.opened = !this.opened;
+  }
+
+  _show() {
+    this._toastEl.style.display = 'block';
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        this._toastEl.classList.add('visible');
+        this.dispatchEvent(new CustomEvent('overlay-opened', { bubbles: true, composed: true }));
+      });
+    });
+  }
+
+  _hide() {
+    this._toastEl.classList.remove('visible');
+    this.dispatchEvent(new CustomEvent('overlay-closed', { bubbles: true, composed: true }));
+    setTimeout(() => {
+      if (!this.opened) {
+        this._toastEl.style.display = 'none';
+      }
+    }, 500);
+  }
+
+  render() {
+    return html`
+      <div id="toast">
+        <slot></slot>
+      </div>
+    `;
+  }
 }
