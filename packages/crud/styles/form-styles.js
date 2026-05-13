4d4e34aad3b05650e0b192210fbeeff8733eef93
midesweb <malvarez@desarrolloweb.com>
2026-05-13 23:47:34 +0200

    change default styles

--- packages/crud/styles/form-styles.js
+++ packages/crud/styles/form-styles.js
@@ -4,8 +4,8 @@ export const formStyles = css`
 :host {
   display: block;
   margin-bottom: 1rem;
-  --dile-modal-width: 100vw;
-  --dile-modal-height: 100vh;
+  --dile-modal-width: 90vw;
+  --dile-modal-height: auto;
   --dile-modal-close-icon-top: 1rem;
   --dile-modal-close-icon-color: #f66;
 }
@@ -15,7 +15,7 @@ h1 {
 }
 @media(min-width: 380px) {
   :host {
-    --dile-modal-width: 90vw;
+    --dile-modal-width: 85vw;
     --dile-modal-height: auto;
     --dile-modal-close-icon-top: 1rem;
     --dile-modal-close-icon-color: #f66;
