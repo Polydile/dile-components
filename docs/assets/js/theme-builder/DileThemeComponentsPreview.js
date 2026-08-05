d00d6b5267c112015b42a74aeaece4db685a220e
midesweb <miguel@escuela.it>
2026-08-05 10:43:58 +0200

    add icons to theme builder

--- docs/assets/js/theme-builder/DileThemeComponentsPreview.js
+++ docs/assets/js/theme-builder/DileThemeComponentsPreview.js
@@ -2,6 +2,9 @@ import { LitElement, html, css } from 'lit';
 import { appsIcon } from "@dile/icons";
 import '@dile/ui/components/button/button-icon.js';
 import '@dile/ui/components/select/select.js';
+import '@dile/iconlib/tabler-icons/invoice.js';
+import '@dile/iconlib/tabler-icons/align-box-bottom-right.js';
+import '@dile/iconlib/phosphor-icons/cloud-sun.js';
 
 /**
  * Renders a sample of real dile-* components styled with the theme
@@ -36,6 +39,43 @@ export class DileThemeComponentsPreview extends LitElement {
         gap: 1rem;
         margin: 1rem 0;
       }
+      div.icons {
+          margin: 1rem 0;
+          display: flex;
+          gap: 0.65rem;
+          align-items: center;
+          padding: 1rem 1.5rem;
+          border-radius: 999px;
+          background-color: var(--dile-gray-very-light-color);
+          justify-content: center;
+      }
+      .icon {
+          --dile-icon-size: 32px;
+      }
+      .primary-icon {
+          --dile-icon-color: var(--dile-on-primary-color);
+          --dile-icon-rounded-background-color: var(--dile-primary-color);
+      }
+      .secondary-icon {
+          --dile-icon-color: var(--dile-on-secondary-color);
+          --dile-icon-rounded-background-color: var(--dile-secondary-color);
+      }
+      .terciary-icon {
+          --dile-icon-color: var(--dile-on-terciary-color);
+          --dile-icon-rounded-background-color: var(--dile-terciary-color);
+      }
+      .invert-primary-icon {
+          --dile-icon-color: var(--dile-primary-color);
+          --dile-icon-rounded-background-color: var(--dile-on-primary-color);
+      }
+      .invert-secondary-icon {
+          --dile-icon-color: var(--dile-secondary-color);
+          --dile-icon-rounded-background-color: var(--dile-on-secondary-color);
+      }
+      .invert-terciary-icon {
+          --dile-icon-color: var(--dile-terciary-color);
+          --dile-icon-rounded-background-color: var(--dile-on-terciary-color);
+      }
     `;
   }
 
@@ -56,6 +96,14 @@ export class DileThemeComponentsPreview extends LitElement {
         <div class="components-row">
           <dile-button>Click here</dile-button>
           <dile-button-icon .icon=${appsIcon}>Control Panel</dile-button-icon>
+        </div>
+        <div class="icons">
+                <dile-tabler-icon-invoice rounded class="icon primary-icon"></dile-tabler-icon-invoice>
+                <dile-tabler-icon-align-box-bottom-right rounded class="icon secondary-icon"></dile-tabler-icon-align-box-bottom-right>
+                <dile-phosphor-icon-cloud-sun rounded class="icon terciary-icon"></dile-phosphor-icon-cloud-sun>
+                <dile-tabler-icon-invoice rounded class="icon invert-primary-icon"></dile-tabler-icon-invoice>
+                <dile-tabler-icon-align-box-bottom-right rounded class="icon invert-secondary-icon"></dile-tabler-icon-align-box-bottom-right>
+                <dile-phosphor-icon-cloud-sun rounded class="icon invert-terciary-icon"></dile-phosphor-icon-cloud-sun>
         </div>
         <dile-input label="Sample input" placeholder="Write something..."></dile-input>
         <dile-select name="select1" label="Selector">
